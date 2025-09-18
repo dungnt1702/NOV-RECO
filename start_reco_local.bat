9ba3854e4bda7cae8fd465e9fdb36aeedd735858
Developer <developer@checkin.reco.vn>
2025-09-19 00:38:20 +0700

    feat: Add virtual host setup for reco.local domain

---
+++ start_reco_local.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Starting NOV-RECO Check-in System...
+echo.
+
+REM Khởi động Django development server
+echo Starting Django server on port 3000...
+start "Django Server" cmd /k "cd /d C:\xampp\htdocs\checkin.reco.vn && C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python311\python.exe manage.py runserver 3000"
+
+REM Đợi một chút để server khởi động
+timeout /t 3 /nobreak >nul
+
+REM Mở browser với reco.local
+echo Opening browser at http://reco.local...
+start http://reco.local
+
+echo.
+echo NOV-RECO is now running at:
+echo - Local: http://reco.local
+echo - Admin: http://reco.local/admin
+echo - Username: admin
+echo - Password: admin123
+echo.
+echo Press any key to exit...
+pause >nul
