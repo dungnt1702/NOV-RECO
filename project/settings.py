40a64d1654a620811faf18a6435544d2e3f395d5
bryannguyen <dungnt.itm@gmail.com>
2025-09-21 22:09:14 +0700

    🔧 Fix EMAIL_PORT int conversion error

--- project/settings.py
+++ project/settings.py
@@ -189,7 +189,7 @@ REST_FRAMEWORK = {
 # Email Configuration
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587") or "587")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "0") == "1"
